eb589f2454477917bd50ab09bf4bc57b50949e47
Carlos Mateus <mateusdev33@gmail.com>
2025-11-23 18:18:30 -0300

    mais ajustes

--- portifolio/src/componentes/about/about.jsx
+++ portifolio/src/componentes/about/about.jsx
@@ -4,21 +4,10 @@ import foto2 from '../../assets/foto2.png'
 import fotoesportes from '../../assets/foto-esportes.jpg'
 export default function About() {
     return (
-        <section className="about py-5 container-fluid" id='about'>
+        <section className="about py-5 px-5 container-fluid" id='about'>
             <h1 className='text-center reveal'>Sobre mim</h1>
-            <div className='reveal d-flex flex-row mx-5 '>
-                <div className="content-text w-75 pt-5 ">
-                    
-                    {/* <p className='about-text text-white pt-5 px-4 align-content-center fs-4'>
-                        Trabalho com HTML, CSS, JavaScript/TypeScript, uso frameworks como Angular e React (este portfólio foi desenvolvido em React).
-                        Uso PostgreSQL e mySQL como banco de dados e no Backend utilizo nodeJS ou Python. Gosto de aprender sobre Cloud Computing, IA e Blockchain.
-                    </p> */}
-                    
-     
-                </div>
-                
-            </div>
-            <div className="tab-container d-flex flex-row justify-content-between">
+            <div className='reveal d-flex flex-row mx-5'>
+                <div className="tab-container d-flex flex-row justify-content-between">
 
                 <input type="radio" name="tab" id="tab1" defaultChecked hidden />
                  <input type="radio" name="tab" id="tab2" hidden />
@@ -29,10 +18,9 @@ export default function About() {
                 <label htmlFor="tab3">Aba 3</label>
              </div>
 
-      {/* Conteúdo */}
              <div className="content d-flex flex-row">
-                <div id="c1" className="tab-content w-75 text-end">
-                 <img className='rounded-circle animate-float img' src={foto} alt="" />
+                <div id="c1" className="tab-content  text-end">
+                 <img className='rounded-3 animate-float img' src={foto} alt="" />
                 <p className="about-text text-white pt-5 px-4 align-content-center fs-4">
                  Desenvolvedor Fullstack em formação no Instituto Federal do Rio Grande do Norte (IFRN), campus Parnamirim. Atualmente, curso Tecnologia em Sistemas para Internet e estou no 4º semestre, com previsão de conclusão em dezembro de 2027.
                 <br />
@@ -40,17 +28,20 @@ export default function About() {
                  Atualmente estou morando no Município de Nisia Floresta e atuo em toda a grande Natal, tenho 23 anos e estou a procura de entrar no mercado de desenvolvimento de software.
                 </p>
             </div>
-             <div id="c2" className="tab-content w-75 text-end">
-                    <img className=' rounded-circle animate-float img' src={foto2} alt="" />
-                    <p className='about-text text-white pt-5 px-4 align-content-center fs-4'>
-                        Meus Hobbies envolvem leitura e atividades fisicas, busco fazer algo que ocupe a minha mente de forma saudável e me elevem fisica e mentalmente como ir à academia, praticar Jiu-jitsu e caminhar. Minha meta é ler pelo menos 10 páginas de um livro por dia, ir todos os dias a academia e conseguir minha primeira graduação no Jiu-jitsu.
-                    </p>
+             <div id="c2" className="tab-content text-end">
+                    <img className=' rounded-3 animate-float img' src={foto2} alt="" />
+                    
             </div>
-                    <div id="c3" className="tab-content w-75">
-                        <img className='rounded-circle animate-float img' src={fotoesportes} alt="" />
+                    <div id="c3" className="tab-content">
+                        <img className='rounded-3 animate-float img' src={fotoesportes} alt="" />
+                        <p className='about-text text-white pt-5 px-4 align-content-center fs-4'>
+                            Meus Hobbies envolvem leitura e atividades fisicas, busco fazer algo que ocupe a minha mente de forma saudável e me elevem fisica e mentalmente como ir à academia, praticar Jiu-jitsu e caminhar. Minha meta é ler pelo menos 10 páginas de um livro por dia, ir todos os dias a academia e conseguir minha primeira graduação no Jiu-jitsu.
+                        </p>
                     </div>
                 </div>
             </div>
+            </div>
+            
     </section>
     )
 }
--- portifolio/src/componentes/contact-card/contact-card.jsx
+++ portifolio/src/componentes/contact-card/contact-card.jsx
@@ -2,9 +2,12 @@ import './contact-card.css'
 export default function ContactCard({icon, title, local}) {
     return (
         <div className='card-contact text-white rounded-4   d-flex flex-column align-items-center justify-content-center fade-in'>
-            <i class={icon}></i>
-            <h3>{title}</h3>
-            <p>{local}</p> 
+            <a href='{}'>
+                <i class={icon}></i>
+                <h3>{title}</h3>
+                <p>{local}</p> 
+            </a>
+           
         </div> 
     )
 }   
