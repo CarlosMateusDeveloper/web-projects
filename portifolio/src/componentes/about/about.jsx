6501a64b6cfa58fcebc3837cdf44631533ed67ef
Carlos Mateus <mateusdev33@gmail.com>
2025-11-28 19:37:53 -0300

    organização dos textos em about.jsx

--- portifolio/src/componentes/about/about.jsx
+++ portifolio/src/componentes/about/about.jsx
@@ -4,24 +4,24 @@ import foto2 from '../../assets/foto2.png'
 import fotoesportes from '../../assets/foto-esportes.jpg'
 export default function About() {
     return (
-        <section className="about py-5 px-5 container-fluid" id='about'>
+        <section className="about p-5 container-fluid" id='about'>
             <h1 className='text-center reveal'>Sobre mim</h1>
-            <div className='reveal d-flex flex-row '>
-                <div className="tab-container d-flex flex-row justify-content-between">
+            <div className='reveal d-flex flex-row pt-5 '>
+                <div className="tab-container d-flex flex-row justify-content-between tab-wrapper">
 
                 <input type="radio" name="tab" id="tab1" defaultChecked hidden />
                  <input type="radio" name="tab" id="tab2" hidden />
                 <input type="radio" name="tab" id="tab3" hidden />
              <div className="tabs">
                  <label className='text-secondary fs-2' htmlFor="tab1">Formação</label>
-                 <label className='text-secondary fs-2' htmlFor="tab2">Eu</label>
+                 <label className='text-secondary fs-2' htmlFor="tab2">Sobre mim</label>
                 <label className='text-secondary fs-2' htmlFor="tab3">Hobbies</label>
              </div>
 
              <div className="content d-flex flex-row ps-5">
                 <div id="c1" className="tab-content text-start ">
                 <p className="about-text text-white pt-2 px-4 align-content-center fs-4 ">
-                 Desenvolvedor Fullstack em formação no Instituto Federal do Rio Grande do Norte (IFRN), campus Parnamirim. Atualmente, curso Tecnologia em Sistemas para Internet e estou no 4º semestre, com previsão de conclusão em dezembro de 2027.
+                 Sou Desenvolvedor Fullstack em formação no Instituto Federal do Rio Grande do Norte (IFRN), no campus de  Parnamirim. <br /> <br /> Cursando Tecnologia em Sistemas para Internet, estou no 4º semestre e a previsão para a minha conclusão é para dezembro de 2026.
                 <br />
                  <br /> {/*em Desenvolvimento fullstack, banco de dados, Cloud Computing, machine learning, Deep Learning e Visão Computacional */}
                 Tenho certificações em plataformas como: <br /> 
@@ -35,13 +35,13 @@ export default function About() {
                 <img className='rounded-3 animate-float img ' src={foto} alt="" />  
             </div>
              <div id="c2" className="tab-content text-end">
-                <p className='about-text text-white pt-2 px-4 align-content-center text-start fs-4 '>  Sou homem, heterossexual, tenho 23 anos e atualmente estou morando no Município de Nisia Floresta e trabalho em todas as modalidades, presencial, Híbrido ou remoto,  ocupo vagas de Desenvolvedor de Software e analista de TI.</p>
+                <p className='about-text text-white pt-2 px-4 align-content-center text-start fs-4 '>  Sou homem, heterossexual, tenho 23 anos e moro no Município de Nisia Floresta. <br /> <br /> Trabalho em todas as modalidades, presencial, Híbrido ou remoto e aceito vagas de estágio ou de Desenvolvedor Junior.</p>
                     <img className=' rounded-3 animate-float img' src={foto2} alt="" />
                     
             </div>
                     <div id="c3" className="tab-content">
                         <p className='about-text text-white pt-2 px-4 align-content-center fs-4'>
-                            Meus Hobbies envolvem leitura e atividades fisicas, busco fazer algo que ocupe a minha mente de forma saudável e me elevem fisica e mentalmente como ir à academia, praticar Jiu-jitsu e caminhar. Minha meta é ler pelo menos 10 páginas de um livro por dia, ir todos os dias a academia e conseguir minha primeira graduação no Jiu-jitsu.
+                            Meus Hobbies envolvem: <br /> <br /> Leitura e atividades fisicas, procuro fazer algo que ocupe a minha mente de forma saudável e me elevem fisica e mentalmente como ir à academia, praticar Jiu-jitsu e ler. <br /> <br /> Minha meta é ler pelo menos 10 páginas de um livro por dia, ir todos os dias a academia e conseguir minha primeira graduação no Jiu-jitsu.
                         </p>
                         <img className='rounded-3 animate-float img' src={fotoesportes} alt="" />
                     </div>
--- portifolio/src/componentes/maindiv/Maindiv.jsx
+++ portifolio/src/componentes/maindiv/Maindiv.jsx
@@ -6,7 +6,7 @@ export default function Maindiv() {
         <div className="home-content reveal d-fex flex-column justify-content-center align-items-center mb-5 pe-md-5">
          <h1 className='text-center text-lg-end '><span>Mateus Gonçalves</span> </h1>  
          <h3 className="typing-text text-center text-lg-end">Desenvolvedor de Software</h3>
-         <p className='text-secondary fs-5 text-end ms-0 ms-md-auto  w-75 '>Desenvolvimento de soluções de software com foco em performance, escalabilidade e design intuitivo. Especialista em React, Node.js e python  para criar aplicações web que impulsionam resultados.</p>
+         <p className='text-secondary fs-5 text-end ms-0 ms-md-auto  w-75 '>Desenvolvimento de soluções de software com foco em performance, escalabilidade. Especialista em React e python  para criar aplicações web que impulsionam resultados.</p>
          <div className="social-icons d-flex justify-content-end gap-3 fs-3 mb-5 ">
             <a className=' d-inline-flex justify-content-center align-items-center border rounded-circle border-danger' href="https://www.linkedin.com/in/carlos-gonçalves-b38701211/" target="_blank"><i class="bi bi-linkedin text-danger w-50"></i></a>
             <a className='d-inline-flex justify-content-center align-items-center border rounded-circle border-danger ' href="https://github.com/CarlosMateusDeveloper" target="_blank"><i class="bi bi-github text-danger"></i></a>
