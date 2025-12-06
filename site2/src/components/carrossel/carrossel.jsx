a5d92d0a44c65c38b72b58f7cc122ab88f3083bc
Carlos Mateus <mateusdev33@gmail.com>
2025-12-06 20:50:02 -0300

    correções em services

--- site2/src/components/carrossel/carrossel.jsx
+++ site2/src/components/carrossel/carrossel.jsx
@@ -1,35 +1,62 @@
-// Use o componente React-Bootstrap (se você o tiver)
-import { Carousel } from 'react-bootstrap'; 
+import { Carousel } from "bootstrap";
+import React, { useState } from "react";
+import Slider from "react-slick";
 
+function Resizable() {
+  const [display, setDisplay] = useState(true);
+  const [width, setWidth] = useState(600);
 
-<Carousel indicators={false}>
-  {/* PRIMEIRO SLIDE (com 3 cards) */}
-  <Carousel.Item>
-    <div className="row">
-      <div className="col-md-4">
-        <SeuCardDeServico />
-      </div>
-      <div className="col-md-4">
-        <SeuCardDeServico />
-      </div>
-      <div className="col-md-4">
-        <SeuCardDeServico />
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  };
+  return (
+    <div className="slider-container">
+      <h2> Resizable Collapsible </h2>
+      <button className="button" onClick={() => setWidth(width + 100)}>
+        {" "}
+        increase{" "}
+      </button>
+      <button className="button" onClick={() => setWidth(width - 100)}>
+        {" "}
+        decrease{" "}
+      </button>
+      <button className="button" onClick={() => setDisplay(!display)}>
+        {" "}
+        toggle{" "}
+      </button>
+      <div
+        style={{
+          width: width + "px",
+          display: display ? "block" : "none"
+        }}
+      >
+        <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
       </div>
     </div>
-  </Carousel.Item>
+  );
+}
 
-  {/* SEGUNDO SLIDE (com os próximos 3 cards) */}
-  <Carousel.Item>
-    <div className="row">
-      <div className="col-md-4">
-        <SeuCardDeServico />
-      </div>
-      <div className="col-md-4">
-        <SeuCardDeServico />
-      </div>
-      <div className="col-md-4">
-        <SeuCardDeServico />
-      </div>
-    </div>
-  </Carousel.Item>
-</Carousel>
+export default Carousel;
--- site2/src/components/servic/services.jsx
+++ site2/src/components/servic/services.jsx
@@ -1,20 +1,23 @@
+import { Carousel } from 'bootstrap'
 import Button from '../button/button'
 import './services.css'
+import '../carrossel/carrossel.jsx'
 export default function Services() {
     return (
-        <section className='px-5' id="services">
-            <div className='services-content container-fluid px-5 d-flex '>
-                <div>
-                    <h6 className='subtitle-services'>OUR SERVICES</h6>
-                    <h1>High Quality Construction Services</h1>
+        <section className=' d-flex flex-column' id="services">
+            <div className='services-content container-fluid px-5 pt-5 mt-5  d-flex '>
+                <div className='px-5 mt-5 me-5 d-flex'>
+                    <div>
+                        <h6 className='subtitle-services'>OUR SERVICES</h6>
+                        <h1>High Quality Construction Services</h1>
+                    </div>
+                     <p className='text-secondary '>We provide multiple services for you, offering confidence and security in construction.</p>
+                      <Button text={"Contact Now"}/>
                 </div>
-                <p className='text-secondary'>We provide multiple services for you, offering confidence and security in construction.</p>
-                <Button text={"Contact Now"}/>
-
+               
+              <Carousel className='carrossel-services' />
             </div>
             <div className="back"></div>
-            <div> 
-            </div>
         </section>
     )
 }
