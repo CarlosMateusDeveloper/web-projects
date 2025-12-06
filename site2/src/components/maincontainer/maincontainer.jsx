79b92edce787fd6f8ffc2a5c9f913ddadb4ff6fb
Carlos Mateus <mateusdev33@gmail.com>
2025-12-06 18:39:24 -0300

    correção em maincontainer.jsx

--- site2/src/components/maincontainer/maincontainer.jsx
+++ site2/src/components/maincontainer/maincontainer.jsx
@@ -7,19 +7,19 @@ export default function maincontainer() {
         <section className="maincontainer container-fluid pt-5 px-5 d-flex flex-row pt-5" id='maincontainer'>
             <div className='d-flex flex-column justify-content-start align-items-start pt-5 mt-3 ms-5 ps-5'>
                 <h1 className="title text-white w-75 ">Create The <br /> Constructions <br /> You Want Here</h1>
-                <p className='text-secondary w-75'>We provide the best home design, construction and maintenance services for you and your family.</p>
+                <p className='text-secondary fs-6 w-75'>We provide the best home design, construction and maintenance services for you and your family.</p>
                 <div className="buttons d-flex gap-3 justify-content-center">
                     <Button text="Our Services"/>
                     <button className='v-projects bg-transparent'>View Projects <i className="bi bi-arrow-right text-danger"></i></button>
                 </div>
                 <div className="numbers d-flex gap-5 pt-4 me-5">
                     <div className=''>
-                    <h3 className='number fs-1'>10+</h3>
+                    <h3 className='number fs-1 '><strong>10+</strong> </h3>
                     <p className='text-white '>Years of <br />Experience</p>
                 </div>
                 <div className='numbers ps-5'>
-                    <h3 className='number fs-1'>200</h3>
-                    <p className='text-white '>Complete <br /> Projects</p>
+                    <h3 className='number fs-1'> <strong>200</strong> </h3>
+                    <p className='text-white fs-6'>Complete <br /> Projects</p>
                 </div>
                 </div>
             </div>
