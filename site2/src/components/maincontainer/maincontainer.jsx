a421022a2b59037b018a92988b126fed8fa4d918
Carlos Mateus <mateusdev33@gmail.com>
2025-12-13 14:21:52 -0300

    conclusão do layout principal

--- site2/src/components/maincontainer/maincontainer.jsx
+++ site2/src/components/maincontainer/maincontainer.jsx
@@ -5,7 +5,7 @@ import homeimage2 from '../../assets/home-img-2.png';
 export default function maincontainer() {
     return (
         <section className="maincontainer container-fluid pt-5 px-5 d-flex flex-row" id='maincontainer'>
-            <div className='d-flex flex-column justify-content-start align-items-start pt-5 mt-3 ms-5 ps-5'>
+            <div className='d-flex flex-column justify-content-start align-items-start pt-4 mt-3 ms-5 ps-5'>
                 <h1 className="title text-white w-75 ">Create The <br /> Constructions <br /> You Want Here</h1>
                 <p className='text-secondary fs-6 w-75'>We provide the best home design, construction and maintenance services for you and your family.</p>
                 <div className="buttons d-flex gap-3 justify-content-center">
@@ -13,7 +13,7 @@ export default function maincontainer() {
                     <button className='v-projects bg-transparent'>View Projects <i className="bi bi-arrow-right text-danger"></i></button>
                 </div>
                 <div className="numbers d-flex gap-5 pt-4 me-5">
-                    <div className=''>
+                    <div>
                     <h3 className='number fs-1 '><strong>10+</strong> </h3>
                     <p className='text-white '>Years of <br />Experience</p>
                 </div>
