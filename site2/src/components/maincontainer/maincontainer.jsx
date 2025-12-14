53f8cf0ac242b0dcf2b257953ea460530a4e3a3f
Carlos Mateus <mateusdev33@gmail.com>
2025-12-14 19:03:52 -0300

    style(maincontainer) add responsive layout for mobile

--- site2/src/components/maincontainer/maincontainer.jsx
+++ site2/src/components/maincontainer/maincontainer.jsx
@@ -7,20 +7,20 @@ export default function maincontainer() {
         <section className="maincontainer container-fluid pt-5 px-5 d-flex flex-row" id='maincontainer'>
             <div className='d-flex flex-column justify-content-start align-items-start pt-4 mt-3 ms-5 ps-5'>
                 <h1 className="title text-white w-75 ">Create The <br /> Constructions <br /> You Want Here</h1>
-                <p className='text-secondary fs-6 w-75'>We provide the best home design, construction and maintenance services for you and your family.</p>
+                <p className='subtitle-main text-secondary fs-6 w-75'>We provide the best home design, construction and maintenance services for you and your family.</p>
                 <div className="buttons d-flex gap-3 justify-content-center">
                     <Button text="Our Services"/>
                     <button className='v-projects bg-transparent'>View Projects <i className="bi bi-arrow-right text-danger"></i></button>
                 </div>
-                <div className="numbers d-flex gap-5 pt-4 me-5">
-                    <div>
-                    <h3 className='number fs-1 '><strong>10+</strong> </h3>
-                    <p className='text-white '>Years of <br />Experience</p>
-                </div>
-                <div className='numbers ps-5'>
-                    <h3 className='number fs-1'> <strong>200</strong> </h3>
-                    <p className='text-white fs-6'>Complete <br /> Projects</p>
-                </div>
+                <div className="numbers d-flex gap-5 pt-lg-4 me-lg-5">
+                    <div className='number '>
+                        <h3 className='number fs-1 '><strong>10+</strong> </h3>
+                        <p className='text-white '>Years of <br />Experience</p>
+                    </div>
+                    <div className='number ps-5'>
+                        <h3 className=' fs-1'> <strong>200</strong> </h3>
+                        <p className='text-white fs-6'>Complete <br /> Projects</p>
+                    </div>
                 </div>
             </div>
             <div className='d-flex justify-content-center pt-5'>
