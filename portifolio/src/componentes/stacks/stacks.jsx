f449ae5225976e5f23c2a48bbd5bd505e17ffed5
Carlos Mateus <mateusdev33@gmail.com>
2025-12-27 20:06:01 -0300

    style(stacks) add other marquee

--- portifolio/src/componentes/stacks/stacks.jsx
+++ portifolio/src/componentes/stacks/stacks.jsx
@@ -9,21 +9,29 @@ export default function Stacks(){
         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" />
         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" />
         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" />
+        <i className="devicon-react-original colored" />
+        <i class="devicon-nodejs-plain-wordmark colored"></i>
           
         <i className="devicon-bootstrap-plain colored" />
         <i className="devicon-tailwindcss-plain colored" />
         <i className="devicon-sass-original colored"></i>
-        <i className="devicon-react-original colored" />
-        <i class="devicon-nodejs-plain-wordmark colored"></i>
-        <i classNamo="devicon-mysql-plain-wordmark colored"></i>
-        <i className="devicon-postgresql-plain-wordmark colored"></i>
-        <i className="back-icon devicon-docker-plain-wordmark colored"></i>
+       
+        
         <i className="devicon-git-plain-wordmark colored"></i>
+        
+        
+      </Marquee>
+      <Marquee speed={120} gradient={false} >
         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg" />
         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/numpy/numpy-original.svg" />
         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/pandas/pandas-original-wordmark.svg" />
         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jupyter/jupyter-original-wordmark.svg" />
-        
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/scikitlearn/scikitlearn-original.svg" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg" />
+        <i className="devicon-postgresql-plain-wordmark colored"></i>
+        <i className="back-icon devicon-docker-plain-wordmark colored"></i>
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linux/linux-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/opencv/opencv-original.svg" />
       </Marquee>
     </div>
     </section>
