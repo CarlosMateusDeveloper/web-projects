5ec45a367e5b86e97dac91cf95e3c28e42b237cc
Carlos Mateus <mateusdev33@gmail.com>
2025-12-27 18:22:01 -0300

    style(cardproject.jsx) change height of mycard

--- portifolio/src/componentes/projects/cardproject/cardproject.jsx
+++ portifolio/src/componentes/projects/cardproject/cardproject.jsx
@@ -4,9 +4,9 @@ export default function CardProject({ title, link, img, description, button}) {
     <div className='my-card d-flex flex-column justify-content-start align-items-center rounded-3'>
         <div className='card-content '>
             <img src={img} alt={title} />
-            <div className='px-2 mt-4 '>
+            <div className='px-2 mt-2 mt-lg-4 '>
               <h3 className='title text-start  '>{title}</h3>
-              <p className='description text-start fs-6 mt-4'>{description}</p>
+              <p className='description text-start mt-0 mt-lg-4'>{description}</p>
               <a className='btn btn-danger' href={link} target="_blank" rel="noreferrer">{button}  </a>
             </div>
         </div>
